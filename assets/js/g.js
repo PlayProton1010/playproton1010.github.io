5a8924f81dbacaa8df018e1f2754f2e35038d6fd
Bwlake <bwigal2@nastudents.org>
2024-04-26 12:45:26 -0400

    Adding JSON code

--- assets/js/g.js
+++ assets/js/g.js
@@ -1,1 +1,36 @@
+const imageContainer = document.getElementById('image-container');
 
+fetch('./assets/json/g.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(image => {
+      const imageElement = document.createElement('a');
+      imageElement.href = image.src;
+
+      const img = document.createElement('img');
+      img.src = image.path;
+      img.alt = image.title || 'ERROR';
+      img.style.width = "90px";
+      img.style.height = "140px";
+      img.className = "classy";
+ 
+      imageElement.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        localStorage.setItem("Iframe", image.src);
+        window.location.href = "./go.html";
+      });
+
+
+      imageElement.appendChild(img);
+
+      imageContainer.appendChild(imageElement);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching JSON data:', error);
+  });
+
+function home() {
+  window.location.href = "./index.html"
+}
