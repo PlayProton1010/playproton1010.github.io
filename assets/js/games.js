7b08ba5e9949c935ed864c56defc569cb4fb5d8c
Bwlake <bwigal2@nastudents.org>
2024-05-03 08:08:43 -0400

    Update games.js

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,41 +1,49 @@
 const imageContainer = document.getElementById('image-container');
 
 fetch('./assets/json/g.json')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(image => {
-      const imageElement = document.createElement('a');
-      imageElement.href = image.src;
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(image => {
+            const imageElement = document.createElement('a');
+            imageElement.href = image.src;
 
-      const img = document.createElement('img');
-      img.src = image.logo;
-      img.alt = image.title || 'ERROR';
-      img.style.width = "150px";
-      img.style.height = "150px";
-      img.className = "classy";
- 
-      imageElement.addEventListener("click", function (event) {
-        event.preventDefault();
-        window.location.href = image.src;
-      });
+            const img = document.createElement('img');
+            img.src = image.logo;
+            img.alt = image.title || 'ERROR';
+            img.style.width = "150px";
+            img.style.height = "150px";
+            img.className = "classy";
 
-      imageElement.appendChild(img);
-      imageContainer.appendChild(imageElement);
+            imageElement.addEventListener("click", function (event) {
+                event.preventDefault();
+                window.location.href = image.src;
+            });
+
+            imageElement.appendChild(img);
+            imageContainer.appendChild(imageElement);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching JSON data:', error);
     });
-  })
-  .catch(error => {
-    console.error('Error fetching JSON data:', error);
-  });
 
 const searchBox = document.getElementById("search-box");
-  const imagesContainer = document.getElementById("image-container");
+const imagesContainer = document.getElementById("image-container");
 
-  searchBox.addEventListener("keyup", function() {
+searchBox.addEventListener("keyup", function() {
     const searchTerm = this.value.toLowerCase();
     const images = imagesContainer.querySelectorAll("img");
 
     images.forEach(function(image) {
-      const altText = image.alt.toLowerCase();
-      image.parentElement.style.visibility = altText.includes(searchTerm) ? "visible" : "hidden";
+        const altText = image.alt.toLowerCase();
+        const parentLink = image.parentElement;
+
+        if (altText.includes(searchTerm)) {
+            parentLink.style.visibility = "visible";
+            parentLink.style.position = "static";
+        } else {
+            parentLink.style.visibility = "hidden";
+            parentLink.style.position = "absolute";
+        }
     });
-  });
+});
