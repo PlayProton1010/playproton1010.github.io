8116f49a94720cc09dda1c3dbfa24a0dc2e8aa8d
Bwlake <bwigal2@nastudents.org>
2024-05-15 08:10:56 -0400

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,24 +10,27 @@ function changeColor() {
 setInterval(changeColor, 1000); 
 
 function e() {
-var url = "https://useproton.github.io/play.html"
-    var urlObj = new window.URL(window.location.href)
-      if (url.substring(0, 8) !== "https://" && url.substring(0, 7) !== "http://") {
+    var url = "https://useproton.github.io/play.html";
+    var urlObj = new window.URL(window.location.href);
+    
+    if (url.substring(0, 8) !== "https://" && url.substring(0, 7) !== "http://") {
         url = "https://" + url.split("https://").pop();
-      } else if (url.substring(0, 7) == "http://") {
+    } else if (url.substring(0, 7) == "http://") {
         url = "https://" + url.split("http://").pop();
-      }
-      win = window.open();
-      win.document.body.style.margin = "0";
-      win.document.body.style.height = "100vh";
-      var iframe = win.document.createElement("iframe");
-      iframe.style.border = "none";
-      iframe.style.width = "100%";
-      iframe.style.height = "100%";
-      iframe.style.margin = "0";
-      iframe.referrerpolicy = "no-referrer";
-      iframe.allow = "fullscreen";
-      iframe.src = url;
-      win.document.body.appendChild(iframe);
-    };
+    }
+    
+    var win = window.open();
+    win.document.body.style.margin = "0";
+    win.document.body.style.height = "100vh";
+    
+    var iframe = win.document.createElement("iframe");
+    iframe.style.border = "none";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.margin = "0";
+    iframe.referrerpolicy = "no-referrer";
+    iframe.allow = "fullscreen";
+    iframe.src = url;
+    
+    win.document.body.appendChild(iframe);
 }
