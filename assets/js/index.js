65a46e8e2be199ea678b95b4344e3fdceab6b757
Bwlake <bwigal2@nastudents.org>
2024-05-15 14:41:15 -0400

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,20 +10,22 @@ function changeColor() {
 setInterval(changeColor, 1000); 
 
 function E() {
-  event.preventDefault()
+event.preventDefault();
 var url = "https://useproton.github.io/play.html";
-  var win = window.open();
-  win.document.body.style.margin = "0";
-  win.document.body.style.height = "100vh";
-  var iframe = win.document.createElement("iframe");
-  iframe.style.border = "none";
-  iframe.style.width = "100%";
-  iframe.style.height = "100%";
-  iframe.style.margin = "0";
-  iframe.referrerpolicy = "no-referrer";
-  iframe.allow = "fullscreen";
-  iframe.src = url;
-  win.document.body.appendChild(iframe);
-  location.replace = "https://google.com"
+var win = window.open();
+win.document.body.style.margin = "0";
+win.document.body.style.height = "100vh";
+var iframe = win.document.createElement("iframe");
+iframe.style.border = "none";
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.margin = "0";
+iframe.referrerpolicy = "no-referrer";
+iframe.allow = "fullscreen";
+iframe.src = url;
+win.document.body.appendChild(iframe);
+location.replace("https://google.com");
+
+
 }
   
