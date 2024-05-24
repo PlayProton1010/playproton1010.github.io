8a021aeebbc48022648766dfefdc882144542b58
Bwlake <bwigal2@nastudents.org>
2024-05-24 10:44:09 -0400

    Create offline.js

---
+++ assets/js/offline.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  './assets/js/index.css',
+  './assets/js/index.js'
+  // Add more URLs of resources you want to cache initially
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Handle requests for the assets/img folder
+  if (event.request.url.includes('./assets/img/') || 
+      event.request.url.includes('./assets/js/') || 
+      event.request.url.includes('./assets/css/') || 
+      event.request.url.includes('./games/')) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          if (response) {
+            return response; // Return the cached resource
+          }
+          // Fetch the resource and cache it
+          return fetch(event.request).then(networkResponse => {
+            return caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            });
+          });
+        })
+    );
+  } else {
+    // Handle other requests
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          return response || fetch(event.request);
+        })
+    );
+  }
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName); // Delete old caches
+          }
+        })
+      );
+    })
+  );
+});
