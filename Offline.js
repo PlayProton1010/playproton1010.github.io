8749206eea6ed0405db2c972a0bcf9ae3b82bae6
bearcattt <bwigal2@nastudents.org>
2024-08-28 10:37:50 -0400

    Update Offline.js

--- Offline.js
+++ Offline.js
@@ -26,30 +26,28 @@ self.addEventListener('fetch', event => {
     caches.match(event.request)
       .then(cachedResponse => {
         if (cachedResponse) {
-          // Check if the resource has been updated on the server
+          
           return fetch(event.request)
             .then(networkResponse => {
               if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
                 return cachedResponse;
               }
 
-              // Update the cache with the latest version of the file
+       
               caches.open(CACHE_NAME).then(cache => {
                 cache.put(event.request, networkResponse.clone());
               });
 
               return networkResponse;
             })
-            .catch(() => cachedResponse); // If network request fails, return the cached response
+            .catch(() => cachedResponse);
         } else {
-          // If not in cache, try to fetch from the network and cache it
           return fetch(event.request)
             .then(networkResponse => {
               if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
                 return networkResponse;
               }
 
-              // Cache the fetched response
               caches.open(CACHE_NAME).then(cache => {
                 cache.put(event.request, networkResponse.clone());
               });
@@ -62,5 +60,5 @@ self.addEventListener('fetch', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim()); // Just claim the clients without deleting caches
+  event.waitUntil(self.clients.claim()); 
 });
