076d709f224582d1559a9b2ab3e1f37e0594bcc3
bearcattt <bwigal2@nastudents.org>
2024-08-28 08:16:11 -0400

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,21 +9,4 @@ function changeColor() {
 
 setInterval(changeColor, 1000); 
 
-function E() {
-event.preventDefault();
-var url = "https://useproton.github.io/play.html";
-var win = window.open();
-win.document.body.style.margin = "0";
-win.document.body.style.height = "100vh";
-var iframe = win.document.createElement("iframe");
-iframe.style.border = "none";
-iframe.style.width = "100%";
-iframe.style.height = "calc(100vh - 100px)"; 
-iframe.style.margin = "0";
-iframe.referrerpolicy = "no-referrer";
-iframe.allow = "fullscreen";
-iframe.src = url;
-win.document.body.appendChild(iframe);
-location.replace("https://google.com");
-}
-  
+
