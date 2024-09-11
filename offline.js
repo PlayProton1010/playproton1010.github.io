acfa6028038aa45acd90407673a22533232ee9c5
bearcattt <bwigal2@nastudents.org>
2024-09-11 09:47:45 -0400

    Update offline.js

--- offline.js
+++ offline.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Proton-V1';
+const CACHE_NAME = 'Proton-V2';
 const urlsToCache = [
   '/',
   './assets/css/index.css',
@@ -8,57 +8,45 @@ const urlsToCache = [
   './assets/json/g.json'
 ];
 
-
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache opened');
-        return cache.addAll(urlsToCache);
-      })
-      .catch(error => console.error('Failed to cache', error))
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
-self.addEventListener('fetch', event => {
-  const requestUrl = new URL(event.request.url);
 
-  if (requestUrl.origin === location.origin && (
-      requestUrl.pathname.startsWith('/assets/img/') || 
-      requestUrl.pathname.startsWith('/assets/js/') || 
-      requestUrl.pathname.startsWith('/assets/css/') || 
-      requestUrl.pathname.startsWith('/games/')
-    )) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => {
-          return response || fetch(event.request).then(networkResponse => {
-            return caches.open(CACHE_NAME).then(cache => {
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        fetch(event.request).then(networkResponse => {
+          if (networkResponse && networkResponse.status === 200) {
+            caches.open(CACHE_NAME).then(cache => {
               cache.put(event.request, networkResponse.clone());
-              return networkResponse;
             });
+          }
+        });
+        return response;
+      }
+      return fetch(event.request).then(networkResponse => {
+        if (networkResponse && networkResponse.status === 200) {
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, networkResponse.clone());
           });
-        }).catch(error => {
-          console.error('Error fetching from network', error);
-          throw error;
-        })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => response || fetch(event.request))
-    );
-  }
+        }
+        return networkResponse;
+      });
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            console.log(`Deleting cache: ${cacheName}`);
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
