feda3aea08c877d20621b5e9278bb9bcbece5ef9
bearcattt <bwigal2@nastudents.org>
2024-09-24 10:50:19 -0400

    Update go.js

--- assets/js/go.js
+++ assets/js/go.js
@@ -1,11 +1,3 @@
-window.onload = function () {
-  var encUrl = localStorage.getItem("Iframe");
-  var iframe = document.createElement("iframe");
-  iframe.className = "iframe";
-  iframe.src = encUrl;
-  document.body.appendChild(iframe);
-  document.body.style.overflow = "hidden";
-};
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker.register("./sw.js", {
@@ -14,3 +6,12 @@ if ("serviceWorker" in navigator) {
     });
 }
   
+
+window.onload = function () {
+  var encUrl = localStorage.getItem("Iframe");
+  var iframe = document.createElement("iframe");
+  iframe.className = "iframe";
+  iframe.src = encUrl;
+  document.body.appendChild(iframe);
+  document.body.style.overflow = "hidden";
+};
