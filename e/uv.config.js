b0c380c9bcd87f31129fde967efd94fbf7f38780
bearcattt <bwigal2@nastudents.org>
2024-09-30 11:01:11 -0400

    Update uv.config.js

--- e/uv.config.js
+++ e/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/p/",
-  bare: "playproton.edura.tk/b/",
+  bare: "https://playproton.edura.tk/b/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/e/uv.handler.js",
