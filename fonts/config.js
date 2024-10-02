e360b8aaf803fc5c70607231ffaab16613d9cd45
bearcattt <bwigal2@nastudents.org>
2024-10-02 10:33:48 -0400

    Update config.js

--- fonts/config.js
+++ fonts/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/jquery/",
-  bare: "https://englishtutore.edurapp.tk/b/",
+  bare: "",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/fonts/handler.js",
