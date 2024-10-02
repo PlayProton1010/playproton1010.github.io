b7a15c85b8bfc6d3702073566d3460157491a2b6
bearcattt <bwigal2@nastudents.org>
2024-10-02 10:40:10 -0400

    Update go.js

--- assets/js/go.js
+++ assets/js/go.js
@@ -7,10 +7,10 @@ if ("serviceWorker" in navigator) {
 }
 
 window.onload = function () {
-  var encUrl = localStorage.getItem("Iframe");
+  var url = localStorage.getItem("Iframe");
   var iframe = document.createElement("iframe");
   iframe.className = "iframe";
-  iframe.src = encUrl;
+  iframe.src = url;
   document.body.appendChild(iframe);
   document.body.style.overflow = "hidden";
 };
