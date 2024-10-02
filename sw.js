07e13a1d409e50d6a687d99a9dea05c8d905c2ff
bearcattt <bwigal2@nastudents.org>
2024-10-02 11:41:51 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-importScripts("/fonts/bundle.js");
-importScripts("/fonts/config.js");
-importScripts("/fonts/sw.js");
+importScripts("./fonts/bundle.js");
+importScripts("./fonts/config.js");
+importScripts("./fonts/sw.js");
 
 const u = new UVServiceWorker();
 const userKey = new URL(location).searchParams.get("userkey");
