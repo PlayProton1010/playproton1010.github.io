8021b854b62de891b6370b47f9260f436dee8523
bearcattt <bwigal2@nastudents.org>
2024-10-09 12:41:18 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,12 @@
-importScripts("./fonts/bundle.js");
-importScripts("./fonts/config.js");
-importScripts("./fonts/sw.js");
+importScripts("./fonts/bundle.js", "./fonts/config.js", "./fonts/sw.js");
 
 const u = new UVServiceWorker();
 const userKey = new URL(location).searchParams.get("userkey");
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    (async () => {
-      if (event.request.url.startsWith(`${location.origin}/jquery/`)) {
-        return await u.fetch(event);
-      }
-
-      return await fetch(event.request);
-    })(),
-  );
+  if (event.request.url.startsWith(`${location.origin}/jquery/`)) {
+    event.respondWith(u.fetch(event));
+  } else {
+    event.respondWith(fetch(event.request));
+  }
 });
